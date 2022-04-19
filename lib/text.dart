aaab16c9c34f04f48793353ebf6a1c77394a77c5
Aamir <csguy.flutter@gmail.com>
2022-04-19 15:04:39 +0500

    Final Commit

---
+++ lib/text.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TextADD extends StatelessWidget {
+  const TextADD({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
